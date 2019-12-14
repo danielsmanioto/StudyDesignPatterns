b3a6a8839d1d9a1371a2c58adf4fc0139233a3c4
Daniel Smanioto <daniel.smanioto@gmail.com>
2019-12-13 21:55:29 -0300

    add Facade

---
+++ Facade/facade-cpu/src/main/java/com/danielsmanioto/facade/cpu/facadecpu/CPU.java
@@ -0,0 +1,14 @@
+public class CPU {
+
+    public void freeze() {
+	System.out.println("freeze");
+    }
+    
+    public void jump(int position) {
+	System.out.println("jump");
+    }	
+
+    public void execute() {
+	System.out.println("execute");
+    }
+}
